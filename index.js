9199c5bdf58f0536e31c1f54f0d2e2987f710666
Finn Terdal <fterdal@gmail.com>
2019-12-30 15:30:04 -0600

    set up socket client and server

--- index.js
+++ index.js
@@ -2,28 +2,21 @@ const express = require('express')
 const html = require('html-template-tag')
 const Bundler = require('parcel-bundler')
 const path = require('path')
+const socketIO = require('socket.io')
 
 const app = express()
-
-// app.get('/', (req, res, next) => {
-//   res.send(html`
-//   <style>
-//     div {
-//       display: flex;
-//       justify-content: center;
-//     }
-//   </style>
-//     <div>
-//       <h1>Hello World</h1>
-//     </div>
-//   `)
-// })
 
 const entry = path.join(__dirname, 'src', 'index.html')
 const bundler = new Bundler(entry)
 app.use(bundler.middleware())
 
 const PORT = 7070
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
+})
+
+const io = socketIO(server)
+
+io.on('connection', (socket) => {
+  console.log('a client connected')
 })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import io from 'socket.io-client'
+
+const socket = io(window.location.origin)
+
+socket.on('connect', () => {
+  console.log('Connected!')
+})
+
 const App = () => {
   return (
     <div className="app">
