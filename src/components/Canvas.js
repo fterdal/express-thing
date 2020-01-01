b1e2ff03999aafe5e9b3228d0ece163ba227574a
Finn Terdal <fterdal@gmail.com>
2020-01-01 14:28:26 -0600

    can draw on canvas event after scrolling

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -5,13 +5,28 @@ const Canvas = () => {
   const handleCanvasClick = e => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext("2d")
+    console.log([canvas.offsetLeft, canvas.offsetTop])
+    // console.log([pageXOffset, pageYOffset])
     ctx.beginPath()
-    ctx.rect(e.clientX - 10, e.clientY - 10, 20, 20)
+    ctx.rect(
+      e.clientX + pageXOffset - 40,
+      e.clientY + pageYOffset - 40,
+      20,
+      20
+    )
+    // console.log(e.clientX - 10 + canvas.offsetLeft)
+    // ctx.rect(
+    //   e.clientX - (canvas.offsetLeft),
+    //   e.clientY - (canvas.offsetRight),
+    //   20,
+    //   20
+    // )
     ctx.stroke()
   }
 
   return (
     <canvas
+      style={{margin: "30px"}}
       className="draw-stuff"
       ref={canvasRef}
       width="300"
