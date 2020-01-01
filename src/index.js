d9e6f143cf02fb0d0bf0d70d90d8a77cd087569f
Finn Terdal <fterdal@gmail.com>
2020-01-01 02:03:54 -0600

    first commit of 2020! :D 🎉 🥳

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-import { ClientSocket, useSocket } from 'use-socketio'
+import React, { useState, useRef } from "react"
+import ReactDOM from "react-dom"
+import { ClientSocket, useSocket } from "use-socketio"
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -8,16 +8,46 @@ const App = () => {
   const socket = useSocket("counter", newCount => {
     setCount(newCount.count)
     setCookies(newCount.cookies)
-
   })
+
+  const canvasRef = useRef(null)
+  const handleClick = e => {
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext("2d")
+    ctx.beginPath()
+    ctx.rect(e.clientX - 10, e.clientY - 10, 20, 20)
+    ctx.stroke()
+    console.log(ctx)
+  }
+
   return (
-    <div className="app">
-      <h1>Hello World</h1>
-      <h2>👩‍💻 {count} people are online</h2>
-      <h2>🍪 {cookies} cookies</h2>
-      <button onClick={() => {socket.emit('add-cookie')}}>Add Cookie</button>
-      <button onClick={() => {socket.emit('subtract-cookie')}}>Subtract Cookie</button>
-    </div>
+    <>
+      <canvas
+        className="draw-stuff"
+        ref={canvasRef}
+        width="300"
+        height="300"
+        onClick={handleClick}
+      />
+      <div className="app">
+        <h2>👩‍💻 {count} people are online</h2>
+        <h2>🍪 {cookies} cookies</h2>
+        <button
+          onClick={() => {
+            socket.emit("add-cookie")
+          }}
+        >
+          Add Cookie
+        </button>
+        <button
+          onClick={() => {
+            socket.emit("subtract-cookie")
+          }}
+        >
+          Subtract Cookie
+        </button>
+      </div>
+    </>
   )
 }
 
@@ -25,5 +55,5 @@ ReactDOM.render(
   <ClientSocket url={window.location.origin}>
     <App />
   </ClientSocket>,
-  document.getElementById('app')
+  document.getElementById("app")
 )
